72914525ffc8dc22001c07be9a607509bbb02080
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-18 22:34:46 -0500

    user can login and logout in backend

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -80,23 +80,28 @@ router.post('/', (req, res) => {
     });
 });
 
-// router.post('/login', (req, res) => {
-//   User.findOne({
-//     where: {
-//       email: req.body.email
-//     }
-//   }).then(dbUserData => {
-//     if (!dbUserData) {
-//       res.status(400).json({ message: 'No user with that email address!' });
-//       return;
-//     }
-
-//     const validPassword = dbUserData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res.status(400).json({ message: 'Incorrect password!' });
-//       return;
-//     }
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that username!' });
+          return;
+        }
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+          
+          res.json({ user: dbUserData, message: 'You are now logged in!' });          
+      });  
+    
+})
 
 //     req.session.save(() => {
 //       // declare session variables
@@ -126,7 +131,7 @@ router.put('/:id', (req, res) => {
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
-    // individualHooks: true,
+    individualHooks: true,
     where: {
       id: req.params.id
     }
