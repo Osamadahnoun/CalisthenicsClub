75bc4dbcfeafafa4ae064517dd0c741e87807cfe
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-20 22:16:15 -0500

    added logo to heroku

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
 
 // turn on routes
 app.use(routes);
