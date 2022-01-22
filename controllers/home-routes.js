35c21498eca10932336559472672bb0d62828ab6
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-22 00:22:35 -0500

    login/logout route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,13 @@ router.get('/workoutLog', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/', (req, res) => {
+    res.render('login');
+  });
+  
+router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+  
+
 module.exports = router;
