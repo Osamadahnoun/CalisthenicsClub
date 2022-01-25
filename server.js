6ecf1bbe58856a9159d4fe575f0c151dce74be67
Osama Dahnoun <omdahnou@ncsu.edu>
2022-01-24 20:28:55 -0500

    fixed logo bug

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'images')));
+// app.use(express.static(path.join(__dirname, 'images')));
 app.use(session(sess));
 
 // turn on routes
